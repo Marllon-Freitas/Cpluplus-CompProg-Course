1ac2f67b5088edf31a804c8b435cffece0e7b5b3
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2023-12-31 10:38:21 -0300

    feat: adding fixation exercise q01 lab02

---
+++ lab-02/fixação/q01.cpp
@@ -0,0 +1,13 @@
+/*
+Escreva um programa para exibir seu nome, sobrenome e endereço na tela.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Petter " << "Parker\n";
+	cout << "Ingram Street, 20\n";
+	cout << "New York";
+	return 0;
+}
