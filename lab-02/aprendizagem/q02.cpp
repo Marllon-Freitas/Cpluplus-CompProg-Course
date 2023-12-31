6ca268e2f3d883052d48d06a61a11734c08db6be
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2023-12-31 15:12:50 -0300

    feat: adding learning exercise q02 lab02

---
+++ lab-02/aprendizagem/q02.cpp
@@ -0,0 +1,28 @@
+/*
+Construa um programa que exiba um retângulo com bordas duplas, semelhante ao
+exibido abaixo.
+
+╔═══════════════════════╗
+║                       ║
+║                       ║
+║                       ║
+║                       ║
+╚═══════════════════════╝
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\n\n";
+	cout << "\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB\n";
+	cout << "\xBA\t\t\t\xBA\n";
+	cout << "\xBA\t\t\t\xBA\n";
+	cout << "\xBA\t\t\t\xBA\n";
+	cout << "\xBA\t\t\t\xBA\n";
+	cout << "\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC\n";
+	cout << "\n\n";
+
+	return 0;
+}
