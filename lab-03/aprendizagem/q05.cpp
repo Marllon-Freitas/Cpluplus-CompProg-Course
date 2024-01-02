96867796f01bfb87f59bc44b13a9b98156af699d
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-02 18:58:49 -0300

    feat: adding learning exercise q05 lab03

--- lab-03/aprendizagem/q05.cpp
+++ lab-03/aprendizagem/q05.cpp
@@ -1,3 +1,12 @@
+/*
+Construa um programa que peça ao usuário o horário atual. Declare variáveis para
+receber a hora e os minutos separadamente. Após receber o horário atual, diga ao
+usuário que o relógio dele está uma hora atrasado, e mostre o horário certo, com a
+hora adiantada em uma hora. 
+Que horas são? 10:54
+O seu relógio está atrasado.
+Agora são 11:54.
+*/
 
 #include <iostream>
 using namespace std;
