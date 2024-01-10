bc4e603b468eaaaf932cc964e3256440b3768240
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-10 06:50:15 -0300

    feat: adding fixation exercise q04 lab07

---
+++ lab-07/fixação/q04.cpp
@@ -0,0 +1,19 @@
+/*
+Escreva um programa que use a instrução cout juntamente com o caractere de
+tabulação para produzir a seguinte saída:
+
+Maça = 20 Unidades
+Melancia = 50 Unidades
+Ameixa = 30 Unidades
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Maça\t\t= 20 Unidades\n";
+	cout << "Melancia\t= 50 Unidades\n";
+	cout << "Ameixa\t\t= 30 Unidades\n";
+	return 0;
+}
