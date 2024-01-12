eca4f89e3e25f480be3324ceb26e64b97b319a7d
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-12 07:36:59 -0300

    feat: adding the binary header file on the first project

---
+++ Trabalho-01/binario.h
@@ -0,0 +1,7 @@
+unsigned short ligarBit(unsigned short  valor, unsigned short posicao);
+unsigned short desligarBit(unsigned short  valor, unsigned short posicao);
+bool testarBit(unsigned short valor, unsigned short posicao);
+unsigned short andBinario(unsigned short a, unsigned short b);
+unsigned short  orBinario(unsigned short a, unsigned short b);
+unsigned short bitsBaixos(unsigned short valor);
+unsigned short bitsAltos(unsigned short valor);
