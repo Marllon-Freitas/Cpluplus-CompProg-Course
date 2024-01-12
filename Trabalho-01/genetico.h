b4e47c46c2cdb7b5fea9dc624f4a1129c81461a3
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-12 07:37:34 -0300

    feat: adding the genetic header file on the first project

---
+++ Trabalho-01/genetico.h
@@ -0,0 +1,5 @@
+bool avaliar(unsigned short valor);
+unsigned short cruzamentoPontoUnico(unsigned short a, unsigned short b);
+unsigned short cruzamentoAritmetico(unsigned short a, unsigned short b);
+unsigned short mutacaoSimples(unsigned short a);
+unsigned short mutacaoDupla(unsigned short a);
