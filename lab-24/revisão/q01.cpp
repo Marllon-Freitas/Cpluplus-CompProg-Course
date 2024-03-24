3d31cffb2e7b6e3dee010e9f6a6d3308b7461b98
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-24 00:14:49 -0300

    feat: adding revision exercise q01 lab24

---
+++ lab-24/revisão/q01.cpp
@@ -0,0 +1,18 @@
+/*
+Use o registro peixe definido abaixo.
+
+struct peixe
+{
+ char nome[20];
+ unsigned peso;
+ float comp;
+};
+
+a. Construa um programa que leia do usuário os dados de um único peixe e
+salve-os em um arquivo binário. Cada execução do programa deve
+acrescentar um peixe ao arquivo.
+
+b. Modifique o programa obtido no item anterior para ler e exibir os peixes
+cadastrados no arquivo binário antes de permitir que o usuário faça um
+novo cadastro.
+*/
