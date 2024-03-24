b73a787fa8f6671a12f0f65bdca6db02b0033e5d
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-03-24 15:28:54 -0300

    feat: adding revision exercise q01 lab24

--- lab-24/revisão/q01.cpp
+++ lab-24/revisão/q01.cpp
@@ -16,3 +16,56 @@ b. Modifique o programa obtido no item anterior para ler e exibir os peixes
 cadastrados no arquivo binário antes de permitir que o usuário faça um
 novo cadastro.
 */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct peixe
+{
+  char nome[20];
+  unsigned peso;
+  float comp;
+};
+
+int main()
+{
+  ifstream fin;
+  peixe p;
+  fin.open("pescado.bin", ios_base::in | ios_base::binary);
+
+  if (!fin.is_open())
+  {
+    cout << "Erro ao abrir o arquivo pescado.bin" << endl;
+    return 1;
+  }
+
+  while (fin.read((char *)&p, sizeof(peixe)))
+  {
+    cout << p.nome << " " << p.peso << " " << p.comp << endl;
+  }
+
+  fin.close();
+  cout << endl;
+
+  ofstream fout;
+  fout.open("pescado.bin", ios_base::out | ios_base::app | ios_base::binary);
+
+  if (!fout.is_open())
+  {
+    cout << "Erro ao abrir o arquivo pescado.bin" << endl;
+    return 1;
+  }
+
+  cout << "Digite o nome do peixe: ";
+  cin >> p.nome;
+  cout << "Digite o peso do peixe: ";
+  cin >> p.peso;
+  cout << "Digite o comprimento do peixe: ";
+  cin >> p.comp;
+
+  fout.write((char *)&p, sizeof(peixe));
+  fout.close();
+
+  return 0;
+}
